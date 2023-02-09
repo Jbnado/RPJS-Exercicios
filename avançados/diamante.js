2c7d2e9c2decad8fe623f651fd6081ce53ac8377
Jbnado <bernardojoao9@gmail.com>
2023-02-09 16:53:58 -0300

    primeiros exercicios

---
+++ avançados/diamante.js
@@ -0,0 +1,15 @@
+// Você deve retornar uma string que pareça um diamante utilizando astericos (*).
+// Retorne null se o input for um número par ou negativo, afinal não é possível construir um diamante com essas medidas.
+// Toda linha deve terminar com um \n.
+// Tamanho é um número ímpar e inteiro.
+
+function diamante(tamanho) {
+  // Seu código aqui :)
+}
+
+// Caso queira mais desafios que tal testar sua função com os seguintes valores:
+// 1 (resultado esperado: "*\n")
+// 3 (resultado esperado: " *\n***\n *\n")
+// 5 (resultado esperado: "  *\n ***\n*****\n ***\n  *\n")
+
+// Quer mais desafios? Faça uma nova pasta e um arquivo com este mesmo nome e resolva o desafio utlizando TypeScript.
---
+++ iniciantes/prestacao-com-juros.js
@@ -0,0 +1,20 @@
+// O valor é o valor total e sem juros.
+// O número de parcelas é o número de vezes que o valor será dividido.
+// O juros é de 5% ao mês.
+// O valor apenas terá juros se o número de parcelas for maior que 5.
+// O valor de cada parcela deve seguir o padrão: 00.00.
+// Retorne o valor de cada parcela com ou sem juros.
+
+function prestacao(valor, parcelas) {
+  // Seu código aqui :)
+}
+
+// Caso queira mais desafios que tal testar sua função com os seguintes valores:
+// 1000, 1
+// 1500, 2
+// 3000, 3
+// 1000, 6
+// 2000, 10
+// 10000, 15
+
+// Quer mais desafios? Faça uma nova pasta e um arquivo com este mesmo nome e resolva o desafio utlizando TypeScript.
