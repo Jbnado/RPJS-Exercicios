5dd7c07c770d2b69f036a7797267cd14cdc077fb
Jbnado <bernardojoao9@gmail.com>
2023-03-08 10:55:13 -0300

    new exercises

---
+++ avançados/construirString.js
@@ -0,0 +1,18 @@
+// Dada duas string a e b
+// Sua missão é calcular quantas vezes a string a pode ser construida usando as letras da string b
+// Não pode usar a mesma letra duas vezes
+// Exemplo: "abc" e "abccba" => 2
+
+function construcaoDeString(a, b) {
+  // Seu código aqui :)
+}
+
+// Caso queira mais desafios que tal testar sua função com os seguintes valores:
+// "abc" e "abccba"
+// "abc" e "abccbaabccba"
+// "abc" e "abccbaabccbaabccba"
+// "sonhar" e "latirpatomiasmacavalopaletasimsopaso"
+// "sonhar" e "latirpatomiasmacavalopaletasimsopasosonharlatirpatomiasmacavalopaletasimsopaso"
+// "sonhar" e "latirpatomiasmacavalopaletasimsopasosonharlatirpatomiasmacavalopaletasimsopasosonharlatirpatomiasmacavalopaletasimsopaso"
+
+// Quer mais desafios? Faça uma nova pasta e um arquivo com este mesmo nome e resolva o desafio utlizando TypeScript ou alguma linguagem no qual queira estudar.
---
+++ iniciantes/stringsLongas.js
@@ -0,0 +1,14 @@
+// Dado um array de strings, retorne um array com todas as strings mais longas
+// Exemplo: ["aba", "aa", "ad", "vcd", "aba"] => ["aba", "vcd", "aba"]
+
+function todasAsStringsLongas(arrayDeStrings) {
+  // seu código aqui :)
+}
+
+// Caso queira mais desafios que tal testar sua função com os seguintes valores:
+// ["aba", "aa", "ad", "vcd", "aba"]
+// ["aba", "aa", "ad", "vcd", "aba", "aba", "aa", "ad", "vcd", "aba"]
+// ["aba", "aa", "ad", "vcd", "aba", "aba", "aa", "ad", "vcd", "aba", "aba", "aa", "ad", "vcd", "aba"]
+// ["sonhar", "latir", "pato", "miasma", "cavalo", "paleta", "sim", "sopa", "so"]
+
+// Quer mais desafios? Faça uma nova pasta e um arquivo com este mesmo nome e resolva o desafio utlizando TypeScript ou alguma linguagem no qual queira estudar.
